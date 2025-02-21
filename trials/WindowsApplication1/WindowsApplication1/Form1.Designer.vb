cf1c86020c2217efad31dd54ecaa226cda40a86e
Normel E. Cagas <nrmlcgs@gmail.com>
2025-02-21 12:25:37 +0800

    TES

---
+++ trials/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,487 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txt3 = New System.Windows.Forms.TextBox()
+        Me.txt4 = New System.Windows.Forms.TextBox()
+        Me.txt5 = New System.Windows.Forms.TextBox()
+        Me.txt6 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txt7 = New System.Windows.Forms.TextBox()
+        Me.txt8 = New System.Windows.Forms.TextBox()
+        Me.txt9 = New System.Windows.Forms.TextBox()
+        Me.txt10 = New System.Windows.Forms.TextBox()
+        Me.hps10 = New System.Windows.Forms.Label()
+        Me.hps9 = New System.Windows.Forms.Label()
+        Me.hps8 = New System.Windows.Forms.Label()
+        Me.hps7 = New System.Windows.Forms.Label()
+        Me.hps6 = New System.Windows.Forms.Label()
+        Me.hps5 = New System.Windows.Forms.Label()
+        Me.hps4 = New System.Windows.Forms.Label()
+        Me.hps3 = New System.Windows.Forms.Label()
+        Me.hps2 = New System.Windows.Forms.Label()
+        Me.hps1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.lblWrittenWorksPS = New System.Windows.Forms.Label()
+        Me.lblWrittenWorksWS = New System.Windows.Forms.Label()
+        Me.lblWWTotal = New System.Windows.Forms.Label()
+        Me.lblTotalHPS = New System.Windows.Forms.Label()
+        Me.lblTotalScore = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(15, 12)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(38, 20)
+        Me.txt1.TabIndex = 0
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(62, 12)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(38, 20)
+        Me.txt2.TabIndex = 1
+        '
+        'txt3
+        '
+        Me.txt3.Location = New System.Drawing.Point(109, 12)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(38, 20)
+        Me.txt3.TabIndex = 2
+        '
+        'txt4
+        '
+        Me.txt4.Location = New System.Drawing.Point(156, 12)
+        Me.txt4.Name = "txt4"
+        Me.txt4.Size = New System.Drawing.Size(38, 20)
+        Me.txt4.TabIndex = 3
+        '
+        'txt5
+        '
+        Me.txt5.Location = New System.Drawing.Point(203, 12)
+        Me.txt5.Name = "txt5"
+        Me.txt5.Size = New System.Drawing.Size(38, 20)
+        Me.txt5.TabIndex = 4
+        '
+        'txt6
+        '
+        Me.txt6.Location = New System.Drawing.Point(15, 38)
+        Me.txt6.Name = "txt6"
+        Me.txt6.Size = New System.Drawing.Size(38, 20)
+        Me.txt6.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(256, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 20)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txt7
+        '
+        Me.txt7.Location = New System.Drawing.Point(62, 38)
+        Me.txt7.Name = "txt7"
+        Me.txt7.Size = New System.Drawing.Size(38, 20)
+        Me.txt7.TabIndex = 6
+        '
+        'txt8
+        '
+        Me.txt8.Location = New System.Drawing.Point(109, 38)
+        Me.txt8.Name = "txt8"
+        Me.txt8.Size = New System.Drawing.Size(38, 20)
+        Me.txt8.TabIndex = 7
+        '
+        'txt9
+        '
+        Me.txt9.Location = New System.Drawing.Point(156, 38)
+        Me.txt9.Name = "txt9"
+        Me.txt9.Size = New System.Drawing.Size(38, 20)
+        Me.txt9.TabIndex = 8
+        '
+        'txt10
+        '
+        Me.txt10.Location = New System.Drawing.Point(203, 38)
+        Me.txt10.Name = "txt10"
+        Me.txt10.Size = New System.Drawing.Size(38, 20)
+        Me.txt10.TabIndex = 9
+        '
+        'hps10
+        '
+        Me.hps10.AutoSize = True
+        Me.hps10.Location = New System.Drawing.Point(441, 225)
+        Me.hps10.Name = "hps10"
+        Me.hps10.Size = New System.Drawing.Size(13, 13)
+        Me.hps10.TabIndex = 76
+        Me.hps10.Text = "0"
+        '
+        'hps9
+        '
+        Me.hps9.AutoSize = True
+        Me.hps9.Location = New System.Drawing.Point(441, 205)
+        Me.hps9.Name = "hps9"
+        Me.hps9.Size = New System.Drawing.Size(13, 13)
+        Me.hps9.TabIndex = 75
+        Me.hps9.Text = "0"
+        '
+        'hps8
+        '
+        Me.hps8.AutoSize = True
+        Me.hps8.Location = New System.Drawing.Point(441, 185)
+        Me.hps8.Name = "hps8"
+        Me.hps8.Size = New System.Drawing.Size(13, 13)
+        Me.hps8.TabIndex = 74
+        Me.hps8.Text = "0"
+        '
+        'hps7
+        '
+        Me.hps7.AutoSize = True
+        Me.hps7.Location = New System.Drawing.Point(441, 165)
+        Me.hps7.Name = "hps7"
+        Me.hps7.Size = New System.Drawing.Size(13, 13)
+        Me.hps7.TabIndex = 73
+        Me.hps7.Text = "0"
+        '
+        'hps6
+        '
+        Me.hps6.AutoSize = True
+        Me.hps6.Location = New System.Drawing.Point(441, 145)
+        Me.hps6.Name = "hps6"
+        Me.hps6.Size = New System.Drawing.Size(13, 13)
+        Me.hps6.TabIndex = 72
+        Me.hps6.Text = "0"
+        '
+        'hps5
+        '
+        Me.hps5.AutoSize = True
+        Me.hps5.Location = New System.Drawing.Point(441, 125)
+        Me.hps5.Name = "hps5"
+        Me.hps5.Size = New System.Drawing.Size(13, 13)
+        Me.hps5.TabIndex = 71
+        Me.hps5.Text = "0"
+        '
+        'hps4
+        '
+        Me.hps4.AutoSize = True
+        Me.hps4.Location = New System.Drawing.Point(441, 105)
+        Me.hps4.Name = "hps4"
+        Me.hps4.Size = New System.Drawing.Size(13, 13)
+        Me.hps4.TabIndex = 70
+        Me.hps4.Text = "0"
+        '
+        'hps3
+        '
+        Me.hps3.AutoSize = True
+        Me.hps3.Location = New System.Drawing.Point(441, 85)
+        Me.hps3.Name = "hps3"
+        Me.hps3.Size = New System.Drawing.Size(19, 13)
+        Me.hps3.TabIndex = 69
+        Me.hps3.Text = "15"
+        '
+        'hps2
+        '
+        Me.hps2.AutoSize = True
+        Me.hps2.Location = New System.Drawing.Point(441, 65)
+        Me.hps2.Name = "hps2"
+        Me.hps2.Size = New System.Drawing.Size(19, 13)
+        Me.hps2.TabIndex = 68
+        Me.hps2.Text = "10"
+        '
+        'hps1
+        '
+        Me.hps1.AutoSize = True
+        Me.hps1.Location = New System.Drawing.Point(441, 45)
+        Me.hps1.Name = "hps1"
+        Me.hps1.Size = New System.Drawing.Size(13, 13)
+        Me.hps1.TabIndex = 67
+        Me.hps1.Text = "5"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(203, 215)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox1.TabIndex = 86
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(156, 215)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox2.TabIndex = 85
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(109, 215)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox3.TabIndex = 84
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(62, 215)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox4.TabIndex = 83
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(15, 215)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox5.TabIndex = 82
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(203, 189)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox6.TabIndex = 81
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(156, 189)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox7.TabIndex = 80
+        Me.ToolTip1.SetToolTip(Me.TextBox7, "Double click to edit HPS")
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(109, 189)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox8.TabIndex = 79
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(62, 189)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox9.TabIndex = 78
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(15, 189)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox10.TabIndex = 77
+        '
+        'lblWrittenWorksPS
+        '
+        Me.lblWrittenWorksPS.AutoSize = True
+        Me.lblWrittenWorksPS.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWrittenWorksPS.Location = New System.Drawing.Point(439, 355)
+        Me.lblWrittenWorksPS.Name = "lblWrittenWorksPS"
+        Me.lblWrittenWorksPS.Size = New System.Drawing.Size(24, 25)
+        Me.lblWrittenWorksPS.TabIndex = 88
+        Me.lblWrittenWorksPS.Text = "0"
+        '
+        'lblWrittenWorksWS
+        '
+        Me.lblWrittenWorksWS.AutoSize = True
+        Me.lblWrittenWorksWS.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWrittenWorksWS.Location = New System.Drawing.Point(439, 380)
+        Me.lblWrittenWorksWS.Name = "lblWrittenWorksWS"
+        Me.lblWrittenWorksWS.Size = New System.Drawing.Size(24, 25)
+        Me.lblWrittenWorksWS.TabIndex = 87
+        Me.lblWrittenWorksWS.Text = "0"
+        '
+        'lblWWTotal
+        '
+        Me.lblWWTotal.AutoSize = True
+        Me.lblWWTotal.Location = New System.Drawing.Point(441, 249)
+        Me.lblWWTotal.Name = "lblWWTotal"
+        Me.lblWWTotal.Size = New System.Drawing.Size(13, 13)
+        Me.lblWWTotal.TabIndex = 89
+        Me.lblWWTotal.Text = "0"
+        '
+        'lblTotalHPS
+        '
+        Me.lblTotalHPS.AutoSize = True
+        Me.lblTotalHPS.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalHPS.Location = New System.Drawing.Point(439, 292)
+        Me.lblTotalHPS.Name = "lblTotalHPS"
+        Me.lblTotalHPS.Size = New System.Drawing.Size(24, 25)
+        Me.lblTotalHPS.TabIndex = 92
+        Me.lblTotalHPS.Text = "0"
+        '
+        'lblTotalScore
+        '
+        Me.lblTotalScore.AutoSize = True
+        Me.lblTotalScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalScore.Location = New System.Drawing.Point(439, 317)
+        Me.lblTotalScore.Name = "lblTotalScore"
+        Me.lblTotalScore.Size = New System.Drawing.Size(24, 25)
+        Me.lblTotalScore.TabIndex = 91
+        Me.lblTotalScore.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(281, 364)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 13)
+        Me.Label3.TabIndex = 93
+        Me.Label3.Text = "Written Works PS"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(281, 389)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 13)
+        Me.Label4.TabIndex = 94
+        Me.Label4.Text = "Written Works WS"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(281, 301)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(143, 13)
+        Me.Label5.TabIndex = 95
+        Me.Label5.Text = "Total Highest Possible Score"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(281, 326)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(62, 13)
+        Me.Label6.TabIndex = 96
+        Me.Label6.Text = "Total Score"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(602, 463)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblTotalHPS)
+        Me.Controls.Add(Me.lblTotalScore)
+        Me.Controls.Add(Me.lblWWTotal)
+        Me.Controls.Add(Me.lblWrittenWorksPS)
+        Me.Controls.Add(Me.lblWrittenWorksWS)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.hps10)
+        Me.Controls.Add(Me.hps9)
+        Me.Controls.Add(Me.hps8)
+        Me.Controls.Add(Me.hps7)
+        Me.Controls.Add(Me.hps6)
+        Me.Controls.Add(Me.hps5)
+        Me.Controls.Add(Me.hps4)
+        Me.Controls.Add(Me.hps3)
+        Me.Controls.Add(Me.hps2)
+        Me.Controls.Add(Me.hps1)
+        Me.Controls.Add(Me.txt10)
+        Me.Controls.Add(Me.txt9)
+        Me.Controls.Add(Me.txt8)
+        Me.Controls.Add(Me.txt7)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txt6)
+        Me.Controls.Add(Me.txt5)
+        Me.Controls.Add(Me.txt4)
+        Me.Controls.Add(Me.txt3)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents txt3 As TextBox
+    Friend WithEvents txt4 As TextBox
+    Friend WithEvents txt5 As TextBox
+    Friend WithEvents txt6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txt7 As TextBox
+    Friend WithEvents txt8 As TextBox
+    Friend WithEvents txt9 As TextBox
+    Friend WithEvents txt10 As TextBox
+    Friend WithEvents hps10 As Label
+    Friend WithEvents hps9 As Label
+    Friend WithEvents hps8 As Label
+    Friend WithEvents hps7 As Label
+    Friend WithEvents hps6 As Label
+    Friend WithEvents hps5 As Label
+    Friend WithEvents hps4 As Label
+    Friend WithEvents hps3 As Label
+    Friend WithEvents hps2 As Label
+    Friend WithEvents hps1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents lblWrittenWorksPS As Label
+    Friend WithEvents lblWrittenWorksWS As Label
+    Friend WithEvents lblWWTotal As Label
+    Friend WithEvents lblTotalHPS As Label
+    Friend WithEvents lblTotalScore As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ trials/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+    Dim hpsSum, scoreSum, percentageScoreSum, weightedScoreSum As Double
+    Dim varhps4 As Integer
+    Private Sub TextBox10_TextChanged(sender As Object, e As EventArgs) Handles TextBox10.TextChanged, TextBox9.TextChanged, TextBox8.TextChanged, TextBox7.TextChanged, TextBox6.TextChanged, TextBox5.TextChanged, TextBox4.TextChanged, TextBox3.TextChanged, TextBox2.TextChanged, TextBox1.TextChanged
+        getTotalScore()
+    End Sub
+
+    Public Sub transferValues()
+
+    End Sub
+
+    Public Sub getTotalScore()
+        Dim varhps1 As Integer = Val(hps1.Text)
+        Dim varhps2 As Integer = Val(hps2.Text)
+        Dim varhps3 As Integer = Val(hps3.Text)
+        varhps4 = Val(hps4.Text)
+        Dim varhps5 As Integer = Val(hps5.Text)
+        Dim varhps6 As Integer = Val(hps6.Text)
+        Dim varhps7 As Integer = Val(hps7.Text)
+        Dim varhps8 As Integer = Val(hps8.Text)
+        Dim varhps9 As Integer = Val(hps9.Text)
+        Dim varhps10 As Integer = Val(hps10.Text)
+        Dim varScoreWW1 As Integer = Val(TextBox10.Text)
+        Dim varScoreWW2 As Integer = Val(TextBox9.Text)
+        Dim varScoreWW3 As Integer = Val(TextBox8.Text)
+        Dim varScoreWW4 As Integer = Val(TextBox7.Text)
+        Dim varScoreWW5 As Integer = Val(TextBox6.Text)
+        Dim varScoreWW6 As Integer = Val(TextBox5.Text)
+        Dim varScoreWW7 As Integer = Val(TextBox4.Text)
+        Dim varScoreWW8 As Integer = Val(TextBox3.Text)
+        Dim varScoreWW9 As Integer = Val(TextBox2.Text)
+        Dim varScoreWW10 As Integer = Val(TextBox1.Text)
+
+        hpsSum = varhps1 + varhps2 + varhps3 + varhps4 + varhps5 + varhps6 + varhps7 + varhps8 + varhps9 + varhps10
+        scoreSum = varScoreWW1 + varScoreWW2 + varScoreWW3 + varScoreWW4 + varScoreWW5 + varScoreWW6 + varScoreWW7 + varScoreWW8 + varScoreWW9 + varScoreWW10
+        percentageScoreSum = Format((scoreSum / hpsSum) * 100, "0.00")
+        weightedScoreSum = Format(percentageScoreSum * 0.4, "0.00")
+        lblTotalHPS.Text = hpsSum.ToString
+        lblTotalScore.Text = scoreSum.ToString
+        lblWrittenWorksPS.Text = percentageScoreSum.ToString
+        lblWrittenWorksWS.Text = weightedScoreSum.ToString
+    End Sub
+
+    Private Sub TextBox7_GotFocus(sender As Object, e As EventArgs) Handles TextBox7.GotFocus
+        ToolTip1.ToolTipTitle = "Highest Possible Score is " & varhps4
+    End Sub
+
+    Private Sub TextBox7_DoubleClick(sender As Object, e As EventArgs) Handles TextBox7.DoubleClick
+        Dim a = InputBox("Enter new HPS for Written Work here", "Written Work 4")
+        hps4.Text = a
+        getTotalScore()
+    End Sub
+    'first trial codes
+    'Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    '    TextBox10.Text = txt1.Text
+    '    TextBox9.Text = txt2.Text
+    '    TextBox8.Text = txt3.Text
+    '    TextBox7.Text = txt4.Text
+    '    TextBox6.Text = txt5.Text
+    '    TextBox5.Text = txt6.Text
+    '    TextBox4.Text = txt7.Text
+    '    TextBox3.Text = txt8.Text
+    '    TextBox2.Text = txt9.Text
+    '    TextBox1.Text = txt10.Text
+    'End Sub
+
+    'Private Sub TextBox8_LostFocus(sender As Object, e As EventArgs) Handles TextBox8.LostFocus
+    '    If Val(TextBox8.Text) > Val(hps3.Text) Then
+    '        'MsgBox("Entered score is greater than declared HPS for Written Work 1: " & hps3.Text, MsgBoxStyle.Critical, "Error")
+    '        TextBox8.Focus()
+    '    End If
+    'End Sub
+
+    'Private Sub TextBox9_TextChanged(sender As Object, e As EventArgs) Handles TextBox9.TextChanged
+    '    lblWWTotal.Text = Val(hps1.Text) + Val(hps2.Text) + Val(hps3.Text)
+    '    scoreSum += Val(TextBox8.Text)
+    '    lblWrittenWorksPS.Text = scoreSum
+    'End Sub
+
+    'Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    'End Sub
+
+    'Private Sub TextBox10_TextChanged(sender As Object, e As EventArgs) Handles TextBox10.TextChanged
+
+    'End Sub
+
+    'Private Sub TextBox8_TextChanged(sender As Object, e As EventArgs) Handles TextBox8.TextChanged
+    '    varhps1 = Val(hps3.Text)
+    '    Dim a, b
+    '    If Val(hps1.Text) < 1 Then
+    '        Do While Val(hps3.Text) < 1
+    '            a = InputBox("Please enter Highest Possible Score: ", "HPS Input for Written Work 1")
+    '            hps3.Text = a
+    '        Loop
+    '    ElseIf Val(TextBox8.Text) > varhps1 Then
+    '        b = MsgBox("Entered score is greater than declared HPS for Written Work 1: " & hps3.Text & "Reenter HPS?", MsgBoxStyle.YesNo & MsgBoxStyle.Critical, "Error")
+    '        If b = vbYes Then
+
+    '        End If
+    '    End If
+    '    lblWWTotal.Text = Val(hps1.Text) + Val(hps2.Text) + Val(hps3.Text)
+    '    scoreSum += Val(TextBox8.Text)
+    '    lblWrittenWorksPS.Text = scoreSum
+    'End Sub
+End Class
